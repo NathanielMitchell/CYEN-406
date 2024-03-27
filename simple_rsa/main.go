6b48145d2d463e8ecd2b92f9dcd5f7edd3cbb12e
Mason Sanchez <mjs070@email.latech.edu>
2024-03-27 17:09:59 -0400

    Updated rsa code

--- simple_rsa/main.go
+++ simple_rsa/main.go
@@ -10,12 +10,13 @@ import (
 	"os"
 )
 
+var DEBUG bool = false
+
 func checkErr(err error) {
 	if err != nil {
 		log.Fatal(err)
 	}
 }
-
 // Takes an argument of string for the public key filename
 // encrypts the message
 // prints to stdout and file named 'secret'
@@ -36,17 +37,40 @@ func encrypt(filename string, msg_filename string) {
 
 	cert, err := x509.ParsePKIXPublicKey(block.Bytes)
 	checkErr(err)
+  f, err := os.OpenFile("secret", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666);
 
-	// message must not be > length of the (pub modulus-11) bytes
-	enc_message, err := rsa.EncryptPKCS1v15(random, cert.(*rsa.PublicKey), msg)
-	checkErr(err)
-
-	fmt.Println("Writing to ./secret...")
-	os.WriteFile("secret", enc_message, 0666)
-
-	fmt.Println("Encrypted Message...")
-	fmt.Println(enc_message)
-
+  key_size := (cert.(*rsa.PublicKey).N.BitLen() + 7) / 8
+  chunk_size := key_size - 11
+  fmt.Printf("Key Size: %d, Chunk size: %d\n", key_size, chunk_size)
+  for i := 0;  i < len(msg); i += chunk_size{  
+    if DEBUG {fmt.Printf("i: %d\n", i)}
+    if len(msg) < (i + chunk_size){
+      enc_message, err := rsa.EncryptPKCS1v15(random, cert.(*rsa.PublicKey), msg[i:len(msg)])
+	    checkErr(err)
+	    if DEBUG {fmt.Println("Writing to ./secret...")}
+      f.Write(enc_message);
+	    if DEBUG {
+        fmt.Println("Encrypted Message...")
+	      fmt.Println(enc_message)
+      }
+    } else{
+      enc_message, err := rsa.EncryptPKCS1v15(random, cert.(*rsa.PublicKey), msg[i:(i + chunk_size)])
+	    checkErr(err)
+	    if DEBUG {fmt.Println("Writing to ./secret...")}
+      f.Write(enc_message)
+      if DEBUG {
+	      fmt.Println("Encrypted Message...")
+	      fmt.Println(enc_message)
+      }
+    }
+    // figure out scope to put this outside the logic
+    /*
+	    fmt.Println("Writing to ./secret...")
+	    os.WriteFile("secret", enc_message, 0666)
+	    fmt.Println("Encrypted Message...")
+	    fmt.Println(enc_message)
+    */
+    }
 }
 
 // Takes in the private keys filename
@@ -55,8 +79,6 @@ func decrypt(filname string, secret_msg_filname string) {
 
     secret_msg, err := os.ReadFile(secret_msg_filname)
     checkErr(err)
-
-	random := rand.Reader
 
 	pemData, err := os.ReadFile(filname)
 	checkErr(err)
@@ -69,12 +91,31 @@ func decrypt(filname string, secret_msg_filname string) {
 	cert, err := x509.ParsePKCS8PrivateKey(block.Bytes)
 	checkErr(err)
 
-	dec_message, err := rsa.DecryptPKCS1v15(random, cert.(*rsa.PrivateKey), secret_msg)
-	checkErr(err)
-
-	fmt.Println("Decrypted Message...")
-	fmt.Println(string(dec_message))
-
+  key_size := (cert.(*rsa.PrivateKey).PublicKey.N.BitLen() + 7) / 8
+  chunk_size := key_size
+  if DEBUG {fmt.Printf("Key Size: %d, Chunk size: %d\n", key_size, chunk_size)}
+  for i := 0;  i < len(secret_msg); i += chunk_size{
+    if DEBUG {fmt.Printf("i: %d\n", i)}
+    if len(secret_msg) < (i + chunk_size){
+      dec_message, err := rsa.DecryptPKCS1v15(nil, cert.(*rsa.PrivateKey), secret_msg[i:len(secret_msg)])
+	    checkErr(err)
+      if DEBUG{
+	    fmt.Println("Decrypted Message...")
+      }
+	    fmt.Println(string(dec_message))
+    } else{
+      dec_message, err := rsa.DecryptPKCS1v15(nil, cert.(*rsa.PrivateKey), secret_msg[i:(i + chunk_size)])
+	    checkErr(err)
+      if DEBUG {
+	      fmt.Println("Decrypted Message...")
+      }
+	    fmt.Println(string(dec_message))
+    }
+    /*
+	  fmt.Println("Decrypted Message...")
+	  fmt.Println(string(dec_message))
+    */
+  }
 }
 
 func main() {
