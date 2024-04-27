9820ce53fb690ddc5d6868d19bcf25aca6b54442
VJMumphrey <vjm007@latech.edu>
2024-04-26 19:21:18 -0500

    push dh work for go conversion

--- dhke/main.go
+++ dhke/main.go
@@ -8,15 +8,17 @@ import (
 	"os"
 )
 
-var P = []byte("00c037c37588b4329887e61c2da3324b1ba4b81a63f9748fed2d8a410c2fc21b1232f0d3bfa024276cfd88448197aae486a63bfca7b8bf7754dfb327c7201f6fd17fd7fd74158bd31ce772c9f5f8ab584548a99a759b5a2c0532162b7b6218e8f142bce2c30d7784689a483e095e701618437913a8c39c3dd0d4ca3c500b885fe3")
-
-var g = 2
-
 func main() {
+
+	primeGuy := "00c037c37588b4329887e61c2da3324b1ba4b81a63f9748fed2d8a410c2fc21b1232f0d3bfa024276cfd88448197aae486a63bfca7b8bf7754dfb327c7201f6fd17fd7fd74158bd31ce772c9f5f8ab584548a99a759b5a2c0532162b7b6218e8f142bce2c30d7784689a483e095e701618437913a8c39c3dd0d4ca3c500b885fe3"
+
+	g := 2
+
 	if len(os.Args) < 3 {
 		fmt.Println("usage: ./main.exe [username] [password]")
 		os.Exit(0)
 	}
+
 	var username string = os.Args[1]
 	var password string = os.Args[2]
 	var randomSeed uint64 = rand.Uint64()
@@ -27,8 +29,6 @@ func main() {
 	sum.Write([]byte(privateKey))
 	X := sum.Sum(nil)
 
-	X = big.NewInt(X)
-
-	//what if I do the guy in python since it'll be easier and then if we have time we can do him in go and figure out the number issues?
+	X = new(big.Int).Exp()
 
 }
